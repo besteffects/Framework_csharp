4765736411598804930728e4fbdbfdf428adc7d7
Vitalii <besteffects@gmail.com>
2019-10-08 00:59:42 -0400

    Added abstract page

--- Framework_csharp/Pages/AbstractPage.cs
+++ Framework_csharp/Pages/AbstractPage.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
 
 namespace Framework_csharp.Pages
 {
-    class AbstractPage
+    public abstract class AbstractTracktorPage
     {
+        protected IWebDriver driver;
+        internal protected AbstractTracktorPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            SeleniumExtras.PageObjects.PageFactory.InitElements(driver, this);
+        }
     }
 }
